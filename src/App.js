5fad4210c825b4a8cf7be27f24189cf38a1602c5
Roberto Valdez <robert.tito94@gmail.com>
2020-09-23 17:44:40 -0500

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import { firebaseConfig } from './firebase'
+
 
 function App() {
+
+  console.log({ firebaseConfig })
+
   return (
+    // BEM
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello Roberto. Let's build the Amazon clone </h1><span>🚗</span> */}
+      {/* Header */}
+      <Header />
+      {/* Home */}
+      <Home />
+      {/* Footer */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: process.env.React_App_Fire_Apikey,
+    authDomain: process.env.React_App_Fire_AuthDomain,
+    databaseURL: process.env.React_App_databaseURL,
+    projectId: process.env.React_App_projectId,
+    storageBucket: process.env.React_App_storageBucket,
+    messagingSenderId: process.env.React_App_messagingSenderId,
+    appId: process.env.React_App_appId
+};
